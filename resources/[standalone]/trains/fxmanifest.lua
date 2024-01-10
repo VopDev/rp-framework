9af9f10f856e9b2bcdeb9e1365fdff9665f73e46
VopDev <142059852+VopDev@users.noreply.github.com>
2024-01-10 10:35:46 -0700

    trains

--- resources/[standalone]/trains/fxmanifest.lua
+++ resources/[standalone]/trains/fxmanifest.lua
@@ -1,19 +1,12 @@
-client_scripts {
-	'config.lua',
-	'Locales/en.lua',
-	'Locales/es.lua',
-	'Locales/fr.lua',
-	'client/function.lua',
-	'client/client.lua'
-}
+resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
+resource_version '1.0.0'
+author 'VenomXNL'
 
-server_scripts {
-	'config.lua',
-	'server/server.lua'
-}
-
-this_is_a_map 'yes'
 
 fx_version 'adamant'
 game 'gta5'
-description 'FiveM Trains System'
+
+client_scripts {
+  "config.lua",
+  "client.lua"
+}
