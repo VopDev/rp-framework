567a4e6b2eaa58403681b1d8c2c459be16de72da
VopDev <142059852+VopDev@users.noreply.github.com>
2024-01-11 18:43:01 -0700

    aa

--- resources/[mercy]/mercy-vehicles/client/cl_keys.lua
+++ resources/[mercy]/mercy-vehicles/client/cl_keys.lua
@@ -216,7 +216,7 @@ RegisterNetEvent('mercy-items/client/used-lockpick', function(IsAdvanced, isBank
         EventsModule.TriggerServer('mercy-ui/server/play-sound-in-distance', {['Position'] = {[1] = EntityCoords.x, [2] = EntityCoords.y, [3] = EntityCoords.z}, ['Distance'] = 5.0, ['MaxDistance'] = 0.20, ['Name'] = 'lockpick', ['Volume'] = 0.7, ['Type'] = 'Spatial'})
         LoopAnimation(true, 'anim@amb@clubhouse@tutorial@bkr_tut_ig3@', 'machinic_loop_mechandplayer')
         --local Outcome = exports['mercy-ui']:StartSkillTest(math.random(5, 8), IsAdvanced and { 1, 2 } or { 5, 10 }, IsAdvanced and { 6000, 12000 } or { 1500, 3000 }, true)
-        local Outcome = exports['bl_ui']:Progress(IsAdvanced and math.random(2, 6) or math.random(5, 8), IsAdvanced and 50 or 65)
+        local Outcome = bl_ui:Progress(IsAdvanced and math.random(2, 6) or math.random(5, 8), IsAdvanced and 50 or 65)
         LoopAnimation(false)
         if not Outcome then
             exports['mercy-ui']:Notify('keys-error', "Failed attempt..", 'error')
